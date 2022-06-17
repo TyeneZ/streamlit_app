1f503eb6282185315dd287043d35efc4468a98f3
Tyene Zanella <79229901+TyeneZ@users.noreply.github.com>
2022-06-17 17:12:29 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 #take the json version of the response and normalize it 
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-return fruityvice_normalized
+    return fruityvice_normalized
     
 streamlit.title('My parents new healthy diner')
 
